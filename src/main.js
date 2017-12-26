c97ac2969cb82d846441780484738a46a2618ab1
Phil <youngphil.gim@gmail.com>
2017-12-26 23:49:17 +0900

    add Channel

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import Home from 'components/Home.vue'
 import Quiz from 'components/Quiz.vue'
+import Channel from 'components/Channel.vue'
 import store from 'store'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/quiz/:level', component: Quiz }
+  { path: '/quiz/:level', component: Quiz },
+  { path: '/channel', component: Channel },
 ]
 
 const router = new VueRouter({
