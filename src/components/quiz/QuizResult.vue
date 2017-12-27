9a459c21496bf6e415972d2076505e5167a57e87
Phil <youngphil.gim@gmail.com>
2017-12-27 22:49:12 +0900

    add QuizResult

---
+++ src/components/quiz/QuizResult.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+    <div class="hero-body has-text-centered">
+      <h1 class="subtitle">결과 8/10</h1>
+      <h2 class="title">80점</h2>
+      <button class="button is-primary">돌아가기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
