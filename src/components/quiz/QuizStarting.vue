78b9bedaa91593e87a1f8494189b25b79facbe51
Phil <youngphil.gim@gmail.com>
2017-12-27 22:49:21 +0900

    add QuizStarting

---
+++ src/components/quiz/QuizStarting.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    <div class="hero-body has-text-centered">
+      <button class="button is-primary is-large">시작하기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
