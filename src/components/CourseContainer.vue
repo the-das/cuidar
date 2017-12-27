869534ee314eec27b75f194e9470f5fbd87b10a7
Phil <youngphil.gim@gmail.com>
2017-12-27 18:57:38 +0900

    add CourseContainer

---
+++ src/components/CourseContainer.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <course-hero></course-hero>
+    <div class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-half is-offset-one-quarter">
+              <course-panel></course-panel>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import CourseHero from './course/CourseHero.vue'
+import CoursePanel from './course/CoursePanel.vue'
+
+export default {
+
+  components: {
+    CourseHero,
+    CoursePanel,
+  },
+}
+</script>
+
+<style>
+
+</style>
