43a1e2a54319661d215e6b1cc265468abcb0b0f1
Phil <youngphil.gim@gmail.com>
2017-12-27 18:58:03 +0900

    add CourseHero

---
+++ src/components/course/CourseHero.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="hero is-primary is-medium">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          The DaS
+        </h1>
+        <h2 class="subtitle">
+          난이도를 선택하세요.
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
