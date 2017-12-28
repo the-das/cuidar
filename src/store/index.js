aa4a69264fb00f02e8374d075630a8128232b0b4
Phil <youngphil.gim@gmail.com>
2017-12-28 23:56:21 +0900

    add getters store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import * as getters from './getters'
 import meta from './modules/meta'
 import quiz from './modules/quiz'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
+  getters,
   modules: {
     meta,
     quiz,
