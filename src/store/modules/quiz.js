bbab2812def3d12df3b2011d31a0de45415835b5
Phil <youngphil.gim@gmail.com>
2017-12-29 17:12:56 +0900

    fixed store quiz

--- src/store/modules/quiz.js
+++ src/store/modules/quiz.js
@@ -3,40 +3,38 @@ import * as types from 'store/types'
 
 const state = {
   content: {
-  /**
-   * "beginner": {
-   *   "title": "다스는 누구꺼?",
-   *   "subject": "초심자",
-   *   "time": "10분",
-   *   "content": [
-   *     {
-   *       "question": "1 + 2 = ?",
-   *       "text": "1 + 1 = 2,\n 1 + 2 = ?,\n 1 + 3 = 4",
-   *       "example": [
-   *         "1",
-   *         "2",
-   *         "3",
-   *         "4"
-   *       ],
-   *       "answer": 3,
-   *       "explanation": "1 + 2 = 3"
-   *     },
-   *     {
-   *       "question": "10 + 1 = ?",
-   *       "example": [
-   *         "10",
-   *         "11",
-   *         "12",
-   *         "13"
-   *       ],
-   *       "answer": 2,
-   *       "explanation": "10 + 1 = 11"
-   *     }
-   *   ]
-   *  },
-   *  "intermediate": {},
-   *  "export": {},
-   */
+    // "beginner": {
+    //   "title": "다스는 누구꺼?",
+    //   "subject": "초심자",
+    //   "time": "10분",
+    //   "content": [
+    //     {
+    //       "question": "1 + 2 = ?",
+    //       "text": "1 + 1 = 2,\n 1 + 2 = ?,\n 1 + 3 = 4",
+    //       "example": [
+    //         "1",
+    //         "2",
+    //         "3",
+    //         "4"
+    //       ],
+    //       "answer": 3,
+    //       "explanation": "1 + 2 = 3"
+    //     },
+    //     {
+    //       "question": "10 + 1 = ?",
+    //       "example": [
+    //         "10",
+    //         "11",
+    //         "12",
+    //         "13"
+    //       ],
+    //       "answer": 2,
+    //       "explanation": "10 + 1 = 11"
+    //     }
+    //   ]
+    // },
+    // "intermediate": {},
+    // "expert": {},
   },
   results: {
     // beginner: [],
@@ -44,19 +42,21 @@ const state = {
     // export: [],
   },
   currentLevel: undefined,
+  currentIndex: undefined,
 }
 
 const getters = {
-  currentQuiz: (state) => state.content[state.currentLevel],
+  currentList: (state) => state.content[state.currentLevel],
+  // currentQuiz: (state) => state.currentLevel && state.content[state.currentLevel],
 }
 
 const mutations = {
-  [types.RECEIVE_QUIZ] (state, {level, quiz}) {
-    state.content[level] = quiz
-    state.results = { ...state.result, [level]: [] }
-  },
   [types.SELECT_LEVEL] (state, {level}) {
     state.currentLevel = level
+  },
+  [types.RECEIVE_QUIZ] (state, {level, quiz}) {
+    state.content[level] = quiz
+    state.results = { ...state.results, [level]: [] }
   },
   [types.ANSWER_QUIZ] (state, {result}) {
     state.results[state.currentLevel].push(result)
