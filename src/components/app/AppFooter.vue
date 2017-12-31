c56aaf3f9566a08e2d46d408711e8114aa157257
Phil <youngphil.gim@gmail.com>
2017-12-31 23:16:58 +0900

    fixed AppFooter

--- src/components/app/AppFooter.vue
+++ src/components/app/AppFooter.vue
@@ -1,12 +1,24 @@
-<template>
-  <footer class="footer">
+<template lang="html">
+  <footer class="footer" v-once>
     <div class="container">
-      <div class="content has-text-centered">
+      <div class="content has-text-left">
+        <p>Colophon</p>
         <p>
-          <strong>The DaS</strong> by <a href="http://jgthms.com">Phil</a>. The source code is licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          Built with <a href="https://vuejs.org/" target="_blank">Vue.js</a>,
+          <a href="http://bulma.io/" target="_blank">Bulma</a> and
+          served on <a href="https://github.com/the-das" target="_blank">GitHub</a>,
+          <a href="http://surge.sh/" target="_blank">Surge</a>.
+          <br/>
+          Text is set in Google Font’s
+          <a href="https://googlefonts.github.io/japanese/" target="_blank">Japanese Early Access</a>
         </p>
+        <p>
+          <em>Copyright © 2017</em>
+          <a href="mailto:youngphil.gim@gmail.com" target="_blank">Phil</a>
+        </p>
+        <a href='https://ko-fi.com/A864L8S' target="_blank">
+          <img height='36' style='border:0px;height:36px;' src='https://az743702.vo.msecnd.net/cdn/kofi4.png?v=f' border='0' alt='Buy Me a Coffee at ko-fi.com' />
+        </a>
       </div>
     </div>
   </footer>
@@ -14,10 +26,8 @@
 
 <script>
 export default {
-
 }
 </script>
 
-<style>
-
+<style lang="css">
 </style>
